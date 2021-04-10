018142dcb2f953ded4dead0375147ff7e99cacd6
vipin cheriyanveetil <vipinc.007@gmail.com>
2021-04-10 09:46:15 +0530

    updated backend api for heroku deployment

--- src/Settings/AppSettings.jsx
+++ src/Settings/AppSettings.jsx
@@ -1,4 +1,5 @@
-const BACKEND_API_URL = "http://localhost:5000/";
+// const BACKEND_API_URL = "http://localhost:5000/";
+const BACKEND_API_URL = "https://polaris-vipinc-backend-api.herokuapp.com/";
 
 export default {
   BACKEND_API_URL: BACKEND_API_URL,
